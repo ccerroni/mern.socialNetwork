41b58fc0fcdc5f3adca407559929eeaac086e274
cristiancerroni <cristian.cerroni@radventure.com>
2020-05-25 11:43:03 -0300

    Returning jwt in user register

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const User = require('../../models/User');
 
@@ -49,24 +51,39 @@ router.post(
           .json({ errors: [{ msg: 'User already exists' }] });
       }
 
-      res.send('User Registered');
+      const salt = await bcrypt.genSalt(10);
+      const avatar = gravatar.url(email, { s: 200, r: 'pg', d: 'mm' });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+
+      // return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 3600 },
+        //this is a callback
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (error) {
       console.error(error.message);
       res.status(500).send('Server Error');
     }
-    const salt = await bcrypt.genSalt(10);
-    const avatar = gravatar.url(email, { s: 200, r: 'pg', d: 'mm' });
-
-    user = new User({
-      name,
-      email,
-      avatar,
-      password
-    });
-    user.password = await bcrypt.hash(password, salt);
-    await user.save();
-    // return jsonwebtoken
-    res.send('User Registered');
   }
 );
 
