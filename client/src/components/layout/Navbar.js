587f268ec2e9d135627739f206cc3270d754a707
cristiancerroni <cristian.cerroni@radventure.com>
2020-06-09 14:08:58 -0300

    Adding styles and starting with React project

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navbar = () => (
+  <nav className="navbar bg-dark">
+    <h1>
+      <a>
+        <i className="fas fa-code"></i> MERN Social Network
+      </a>
+    </h1>
+    <ul>
+      <li>Developers</li>
+      <li>Register</li>
+      <li>Login</li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
