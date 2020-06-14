c9baecafb4500b28e68a5513886822b575802687
cristiancerroni <cristian.cerroni@radventure.com>
2020-06-14 18:35:07 -0300

    Adding Login part to Client

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -5,7 +5,10 @@ import {
   REGISTER_FAIL,
   REGISTER_SUCCESS,
   USER_LOADED,
-  AUTH_ERROR
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT
 } from './types';
 
 // Register User
@@ -24,6 +27,7 @@ export const register = ({ name, email, password }) => async dispatch => {
       type: REGISTER_SUCCESS,
       payload: res.data
     });
+    dispatch(loadUser());
   } catch (err) {
     const errors = err.response.data.errors;
     if (errors) {
@@ -52,4 +56,42 @@ export const loadUser = () => async dispatch => {
       type: AUTH_ERROR
     });
   }
+};
+
+// Login User
+export const login = (email, password) => async dispatch => {
+  const config = {
+    headers: {
+      'Content-type': 'application/json'
+    }
+  };
+
+  const body = JSON.stringify({ email, password });
+
+  try {
+    const res = await axios.post('/api/auth', body, config);
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data
+    });
+
+    dispatch(loadUser());
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: LOGIN_FAIL
+    });
+  }
+};
+
+// Logout / Clear Profile
+
+export const logout = () => dispatch => {
+  dispatch({
+    type: LOGOUT
+  });
 };
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../../actions/auth';
 
-const Navbar = () => (
-  <nav className="navbar bg-dark">
-    <h1>
-      <Link to="/">
-        <i className="fas fa-code"></i> MERN Social Network
-      </Link>
-    </h1>
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const authLinks = (
     <ul>
       <li>
-        <Link to="/developers">Developers</Link>
+        <a onClick={logout} href="#!">
+          <i className="fas fa-sign-out-alt"></i>{' '}
+          <span className="hide-sm">Logout</span>
+        </a>
+      </li>
+    </ul>
+  );
+
+  const guessLinks = (
+    <ul>
+      <li>
+        <Link to="/#!">Developers</Link>
       </li>
       <li>
         <Link to="/register">Register</Link>
@@ -19,7 +28,28 @@ const Navbar = () => (
         <Link to="/login">Login</Link>
       </li>
     </ul>
-  </nav>
-);
+  );
+  return (
+    <nav className="navbar bg-dark">
+      <h1>
+        <Link to="/">
+          <i className="fas fa-code"></i> MERN Social Network
+        </Link>
+      </h1>
+      {!loading && (
+        <Fragment>{isAuthenticated ? authLinks : guessLinks}</Fragment>
+      )}
+    </nav>
+  );
+};
 
-export default Navbar;
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, { logout })(Navbar);
